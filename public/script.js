5653563f481485b7a0a631403479398ab56ba8b4
Victor de Souza Gomes Martins <x506821@rede.sp>
2021-07-01 13:38:00 -0300

    first commit

---
+++ public/script.js
@@ -0,0 +1,5 @@
+/**
+ * Babel: Converter (transpilar) código mais atualizado para um código que o browser entenda
+ * Webpack: Pra cada tipo de arquivo (.js, .css, .png) converte o código de uma maneira o browser entenda
+ *  - Loaders: babel-loader, css-loader, image-loader
+ */
